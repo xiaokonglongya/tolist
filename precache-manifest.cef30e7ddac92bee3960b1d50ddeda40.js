5e996db2ebbba1f1917dfd9717f350ad48f958eb
王博文 <1363931801@qq.com>
2019-08-19 18:57:17 +0800

    18.03.01

---
+++ precache-manifest.cef30e7ddac92bee3960b1d50ddeda40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44c9cc39ed6f10ad66c568ecf2b05451",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8fc9d21ee478c23f4789",
+    "url": "/static/css/2.93e9ecf4.chunk.css"
+  },
+  {
+    "revision": "31b3447f814fed8279fd",
+    "url": "/static/css/main.c480fa6f.chunk.css"
+  },
+  {
+    "revision": "8fc9d21ee478c23f4789",
+    "url": "/static/js/2.11b0e110.chunk.js"
+  },
+  {
+    "revision": "31b3447f814fed8279fd",
+    "url": "/static/js/main.88ede58c.chunk.js"
+  },
+  {
+    "revision": "5873fff0003bbf8ef3e3",
+    "url": "/static/js/runtime~main.2c7e4ca4.js"
+  }
+]);
