4942456e8761af3c68eee4693b25eaeea4005d81
王博文 <1363931801@qq.com>
2019-08-19 19:09:21 +0800

    todolist

--- README.md
+++
@@ -1,2 +0,0 @@
-# tolist
-react-demo
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cef30e7ddac92bee3960b1d50ddeda40.js"
+  "//precache-manifest.8e8de6e2dad11d1fce6f9b2fa0dc002f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("//index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
